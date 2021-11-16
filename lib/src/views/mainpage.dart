d1d4232051ac61a23f12f2c4857e0dd7f8c6931b
Joan Sebastián Giraldo Morales <sebastiangiraldo3@gmail.com>
2021-11-16 11:39:26 -0500

    Modificación del main
    Se modifico el archivo Main, eliminando el código por defecto de Flutter, agregando la plantilla mate app y generando las rutas del proyecto

---
+++ lib/src/views/mainpage.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class MainPage extends StatelessWidget {
+  const MainPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor:const Color.fromRGBO(27, 27, 27, 1),
+        body: Container(
+          
+        ),
+      ),
+    );
+  }
+}
